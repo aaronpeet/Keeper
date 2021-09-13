34df41a2e73757a55eb5fc602da3a6b9ef84a4e0
aaronpeet <aaronpeet@icloud.com>
2021-09-12 20:57:11 -0600

    vaultKeep create is passing postman

--- Keeper/Repositories/VaultKeepsRepository.cs
+++ Keeper/Repositories/VaultKeepsRepository.cs
@@ -1,7 +1,28 @@
+using System.Data;
+using Keeper.Models;
+using Dapper;
+
 namespace Keeper.Repositories
 {
     public class VaultKeepsRepository
     {
-        
+        private readonly IDbConnection _db;
+        public VaultKeepsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        internal VaultKeep Create(VaultKeep newVaultKeep)
+        {
+            string sql = @"
+            INSERT INTO vaultKeeps
+            (creatorId, vaultId, keepId)
+            VALUES
+            (@CreatorId, @VaultId, @KeepId);
+            SELECT LAST_INSERT_ID()
+            ;";
+            newVaultKeep.Id = _db.ExecuteScalar<int>(sql, newVaultKeep);
+            return newVaultKeep;
+        }
     }
 }
--- Keeper/Services/VaultKeepsService.cs
+++ Keeper/Services/VaultKeepsService.cs
@@ -1,7 +1,19 @@
+using Keeper.Models;
+using Keeper.Repositories;
+
 namespace Keeper.Services
 {
     public class VaultKeepsService
     {
-        
+        private readonly VaultKeepsRepository _vaultKeepsRepo;
+        public VaultKeepsService(VaultKeepsRepository vaultKeepsRepo)
+        {
+            _vaultKeepsRepo = vaultKeepsRepo;
+        }
+
+        internal VaultKeep Create(VaultKeep newVaultKeep)
+        {
+            return _vaultKeepsRepo.Create(newVaultKeep);
+        }
     }
 }
